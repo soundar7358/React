872843b2ed0953c4e77e2c0662fa148de9876a9c
soundar7358 <soundar7358@gmail.com>
2024-02-25 15:25:47 +0530

    first_commit

---
+++ Price_card/price_card/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PricingSection from './PricingSection';
+
+function App() {
+  return (
+    <div>
+      <PricingSection />
+    </div>
+  );
+}
+
+export default App;
